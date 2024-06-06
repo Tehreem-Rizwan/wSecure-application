b897d1881e74985c12ede7860acc4df258d8e160
Tehreem Rizwan <its.tehreem2830@gmail.com>
2024-06-06 23:45:46 +0500

    windows

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\wSecure-1\\fyp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\src\\flutter"
+  "PROJECT_DIR=E:\\wSecure-1\\fyp"
+  "FLUTTER_ROOT=E:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\wSecure-1\\fyp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\wSecure-1\\fyp"
+  "FLUTTER_TARGET=E:\\wSecure-1\\fyp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNDgxN2M5OWM5ZmQ0OTU2ZjI3NTA1MjA0ZjdlMzQ0MzM1ODEwYWVkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\wSecure-1\\fyp\\.dart_tool\\package_config.json"
+)
